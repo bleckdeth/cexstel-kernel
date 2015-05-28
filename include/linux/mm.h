cae4103655ce9951b4ffc37b3dfe6eb4bbb823b9
srfarias <ericfari15@gmail.com>
2015-05-28 12:20:48 -0300

    Enable Snappy

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1425,13 +1425,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-<<<<<<< HEAD
 #define VM_MAX_READAHEAD	2048	/* kbytes */
 #define VM_MIN_READAHEAD	32	/* kbytes (includes current page) */
-=======
-#define VM_MAX_READAHEAD   2048 /* kbytes */
-#define VM_MIN_READAHEAD     16 /* kbytes (includes current page) */
->>>>>>> 9169677... mm: Added filesystem dynamic read-ahead (Credits to Chad Gooldman)
 
 extern unsigned long max_readahead_pages;
 
